0e9503035283c934a4fa4a4bdc8e00b58b089a56
MichaelHP <michael.falkner@gmail.com>
2017-05-15 13:08:47 -0500

    Added "test scene", tested Gaze.cs with it, works!  Crashing on the other scene, need to investigate.  Cannot see object, need to investigate.

---
+++ JengaGame/Assets/Gaze.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gaze : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(
+                Camera.main.transform.position,
+                Camera.main.transform.forward,
+                out hitInfo,
+                20.0f,
+                Physics.DefaultRaycastLayers))
+
+            print("hit");
+
+
+        {
+            // If the Raycast has succeeded and hit a hologram
+            // hitInfo's point represents the position being gazed at
+            // hitInfo's collider GameObject represents the hologram being gazed at
+        }
+    }
+}
