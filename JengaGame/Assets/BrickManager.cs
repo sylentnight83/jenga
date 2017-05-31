5920c5ea5d53e9e92994f845d35479e8e1d6f0c8
Id Raja <iraja@nerdery.com>
2017-05-31 15:28:23 -0500

    fix the main camera tag issue

--- JengaGame/Assets/BrickManager.cs
+++ JengaGame/Assets/BrickManager.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BrickManager : MonoBehaviour {
-    public void OnGaze() {
-        print("On");
-        // GetComponent<Renderer>().material.color = new Color(0, 255, 0); //C sharp
+    void OnSelect()
+    {
+        print("On Select");
+        GetComponent<Renderer>().material.color = new Color(0, 255, 0); //C sharp
     }
 
-    public void OffGaze() {
-        // print("Off");
+    void OnGaze()
+    {
+
     }
 }
